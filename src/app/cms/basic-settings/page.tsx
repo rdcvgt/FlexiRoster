d70f9035158b41290f2258bd7199c3396a52ae98
finleypeng <weilllyox1020@gmail.com>
2024-09-18 08:49:33 +0800

    feat: implement dialog component

--- src/app/cms/basic-settings/page.tsx
+++ src/app/cms/basic-settings/page.tsx
@@ -1,5 +1,20 @@
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 
-export default function Home() {
-	return <Box>basic-settings</Box>;
+export default function BasicSettings() {
+	return (
+		<>
+			<Box>
+				<Box sx={{ display: "flex", justifyContent: "space-between" }}>
+					<Typography variant="h4">職等</Typography>
+					<Button variant="contained">新增</Button>
+				</Box>
+			</Box>
+
+			{/* <Divider sx={{ mt: 2, mb: 2 }} /> */}
+			{/* <Box>
+				<Typography variant="h4">工作技能</Typography>
+				<Button variant="contained">新增</Button>
+			</Box> */}
+		</>
+	);
 }
---
+++ src/components/Dialog/index.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { useIntl } from "react-intl";
+
+type Props = {
+	children: React.ReactNode;
+	size?: "xs" | "sm" | "md" | "lg" | "xl";
+	titleId: string;
+};
+
+export default function MaxWidthDialog({
+	children,
+	size = "md",
+	titleId,
+}: Props) {
+	const [open, setOpen] = React.useState(false);
+
+	const { formatMessage } = useIntl();
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	return (
+		<>
+			<Dialog fullWidth maxWidth={size} open={open} onClose={handleClose}>
+				<DialogTitle>{formatMessage({ id: titleId })}</DialogTitle>
+				<DialogContent>{children}</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose} variant="outlined">
+						Close
+					</Button>
+					<Button>Save</Button>
+				</DialogActions>
+			</Dialog>
+		</>
+	);
+}
